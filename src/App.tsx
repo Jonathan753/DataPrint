64bfccad37beed04c906a77987ede58ffbe2318f
Jonathan Érik <jonathan753_21@hotmail.com>
2025-08-27 13:56:51 -0300

    Adicionou o container global

--- src/App.tsx
+++ src/App.tsx
@@ -22,23 +22,25 @@ function App() {
         <main className="flex">
           <Sidebar>
             <SidebarItem to="/" icon={<LayoutDashboard size={20} />} text="Adicionar Cliente" />
-            <SidebarItem to="add-user" icon={<BarChart3 size={20} />} active text="Adicionar Serviço/Produto" />
+            <SidebarItem to="add-user" icon={<BarChart3 size={20} />} active text="Listar Clientes" />
+            <SidebarItem to="add-user" icon={<BarChart3 size={20} />} alert text="Adicionar Serviço/Produto" />
             <SidebarItem to="add-service" icon={<UserCircle size={20} />} text="Nota Rápida" />
             <hr className="my-3" />
             <SidebarItem to="teste" icon={<Settings size={20} />} text="Meus Dados" />
           </Sidebar>
           {/* conteudo da pagina */}
-          <div className='p-4'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/add-user' element={<AddUser />} />
-              <Route path='/add-service' element={<AddService />} />
-            </Routes>
+          <div className='flex-1 h-screen overflow-y-auto p-8'>
+            <div className='max-w-4xl mx-auto'>
+              <div className="bg-blue-100 rounded-xl p-5 pl-14">
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/add-user' element={<AddUser />} />
+                  <Route path='/add-service' element={<AddService />} />
+                </Routes>
+              </div>
+              {/* fim do conteudo da pagina */}
+            </div>
           </div>
-          <div className="p-6">
-            <h1 className="text-2xl font-bold">Conteúdo Principal</h1>
-          </div>
-          {/* fim do conteudo da pagina */}
         </main>
 
       </BrowserRouter>
--- src/pages/AddUser.tsx
+++ src/pages/AddUser.tsx
@@ -1,7 +1,10 @@
 const AddUser = () => {
     return (
         <>
-            <h1 className="p-1.5 text-3xl">Adicionar Cliente</h1>
+        <div className="mb-8">
+            <h1 className="text-3xl font-bold text-gray-800">Dashboard</h1>
+            <p className="text-gray-500 mt-1">Bem-vindo de volta, Dev!</p>
+          </div>
             <div className="bg-blue-100 rounded-xl p-5 pl-14">
                 <div className="mt-1.5 ml-1.5">
                     <input className="entrada-texto" type="text" />
