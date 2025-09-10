c0a43964e5c5d8283c6c642b5136b43c943b0d5f
Jonathan Érik <jonathan753_21@hotmail.com>
2025-09-10 15:46:49 -0300

    Add new buttons

---
+++ src/components/ButtonNota.tsx
@@ -0,0 +1,20 @@
+import { Save } from "lucide-react";
+
+interface ButtonProps {
+    nome: string;
+    img: string;
+    rota: string;
+}
+
+const Button = () => {
+    // Fazer o botao de salvar 
+    return (
+        <>
+            <button type="submit" className="px-6 py-2 flex gap-2 bg-indigo-600 text-white rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <Save size={20} /> Nota
+            </button>
+        </>
+    )
+}
+
+export default Button
